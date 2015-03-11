019da8a7a4a0f82feadeb26c2ae084038f7f2941
Amitav Roy <reachme@amitavroy.com>
2015-03-11 11:56:56 +0530

    Laravel 5 init

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+/**
+ * Created by PhpStorm.
+ * User: amitav
+ * Date: 11/3/15
+ * Time: 9:21 AM
+ */
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->truncate();
+
+        $user = new User;
+        $user->name = 'Amitav Roy';
+        $user->email = 'amitav.roy@focalworks.in';
+        $user->password = Hash::make('password');
+        $user->userType = 'normal';
+        $user->userId = '0';
+        $user->save();
+    }
+}
