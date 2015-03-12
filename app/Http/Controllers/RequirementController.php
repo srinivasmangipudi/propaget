3e594a42d95cfb23b10ade914838807cc0ac47e3
Amitav Roy <reachme@amitavroy.com>
2015-03-12 14:37:21 +0530

    Event created and handled for Requirement. Right now called at the controller after save. Need to make it automatic inside the model to make it global.

--- app/Http/Controllers/RequirementController.php
+++ app/Http/Controllers/RequirementController.php
@@ -1,11 +1,13 @@
 <?php namespace App\Http\Controllers;
 
+use App\Events\EventRequirementAdded;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use AdamWathan\BootForms\Facades\BootForm;
 
 use App\Requirement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
 
 class RequirementController extends Controller {
 
@@ -15,14 +17,15 @@ class RequirementController extends Controller {
 
     public function postSaveRequirement(Request $request) {
         $postData = $request->input();
-//        dd($postData);
 
         $req = new Requirement();
         $req->agentId = 1;
         $req->clientId = 1;
         $req->location = $postData['location'];
         $req->area = $postData['area'];
-        $req->save();
+//        $req->save();
+
+        \Event::fire(new EventRequirementAdded(1, $req));
 
         return redirect()->route('addRequirement');
     }
