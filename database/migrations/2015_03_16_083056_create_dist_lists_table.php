8ae2c65f0411a3a2f4efc52829f953042d01019e
Amitav Roy <reachme@amitavroy.com>
2015-03-16 14:17:25 +0530

    Worked on the migrations for the Distribution list and member table.

---
+++ database/migrations/2015_03_16_083056_create_dist_lists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistListsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::dropIfExists('dist_lists');
+		Schema::create('dist_lists', function(Blueprint $table)
+		{
+			$table->increments('id')->comment('Unique identifier for distribution list');
+            $table->string('name')->comment('Name of the distribution list');
+            $table->integer('createdBy')->unsigned()->comment('User id who created the list');
+			$table->timestamps();
+
+            $table->index('createdBy');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::dropIfExists('dist_lists');
+	}
+
+}
---
+++ database/migrations/2015_03_16_083712_DistListMembers.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DistListMembers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::dropIfExists('dist_list_members');
+
+        Schema::create('dist_list_members', function(Blueprint $table)
+        {
+            $table->integer('distListId')->unsigned()->comment('The distribution list id');
+            $table->integer('userId')->unsigned()->comment('The user id who is member of the group');
+            $table->timestamps();
+
+            $table->index('distListId');
+            $table->index('userId');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::dropIfExists('dist_list_members');
+	}
+
+}
