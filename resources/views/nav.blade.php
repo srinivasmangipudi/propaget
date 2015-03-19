bbfa9b9f18ea4ac9651fc5d6cf2b18754346f43c
komal <kom.savla@gmail.com>
2015-03-19 12:41:40 +0530

    Removed li from nav

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -26,13 +26,6 @@
                             <li><a href="{{url('properties#/add')}}">Add Property</a></li>
                         </ul>
                     </li>
-                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Properties <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{url('properties')}}">View</a></li>
-                            <li><a href="{{url('properties#/add')}}">Add</a></li>
-                        </ul>
-                    </li>
                 @endif
             </ul>
 
