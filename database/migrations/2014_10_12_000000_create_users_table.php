6460e3f6479eb12faaa25a5f47af788a9a68b1a2
urmila-patel <urmila.patel@focalworks.in>
2015-04-01 15:26:40 +0530

    Change Column Name in Users Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,14 +18,14 @@ class CreateUsersTable extends Migration {
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('name');
-            $table->string('phoneNumber', 20);
+            $table->string('phone_number', 20);
             $table->string('email')->unique();
             $table->string('password', 60);
-            $table->string('userId')->default("0");
+            $table->string('user_id')->default("0");
             $table->enum('role', array('agent', 'client','anonymous'))->default('anonymous')->comment('Role of User in System');
             $table->string('experience');
             $table->text('summary');
-            $table->enum('userType', array('facebook', 'google', 'normal'))->default('normal')->comment('How the user was created. Facebook, Google etc');
+            $table->enum('user_type', array('facebook', 'google', 'normal'))->default('normal')->comment('How the user was created. Facebook, Google etc');
             $table->rememberToken();
             $table->timestamps();
             $table->enum('status', array(0,1,2))->default(1)->comment('The user status 0: Inactive 1: Active 2: Banned');
